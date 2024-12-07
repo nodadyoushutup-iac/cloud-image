37ed0c2c1d1c045538ac12f601c3c929795ae262
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-07 01:17:16 -0500

    0.1.3

--- script/logger.sh
+++ script/logger.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /opt/logger
 
-log_info "Creating permanent logger script"
+echo "[INFO] Creating permanent logger script"
 if [ -f /tmp/logger.sh ]; then
     mv /tmp/logger.sh /opt/logger/logger.sh
 else
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-log_info "Setting logger script permissions"
-chmod 755 /tmp/logger.sh
+echo "[INFO] Setting logger script permissions"
+chmod 777 /tmp/logger.sh
 
 echo "logger.sh has been successfully moved to /opt/logger and permissions set"
