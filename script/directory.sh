ea358fe4f78a5bd231e36780b0730eb8648bf072
nodadyoushutup <admin@nodadyoushutup.com>
2024-12-07 00:44:39 +0000

    0.1.2

--- script/directory.sh
+++ script/directory.sh
@@ -7,7 +7,4 @@ log_info "Starting directory creation"
 log_info "Creating directory structure: /mnt/efs"
 mkdir -p /mnt/efs || log_error "Failed to create directory /mnt/efs"
 
-# log_info "Changing ownership of /mnt/efs to user 'apps'"
-# chown apps:apps /mnt/efs || log_error "Failed to change ownership of /mnt/efs to 'apps'"
-
 log_info "Directory creation completed successfully!"
