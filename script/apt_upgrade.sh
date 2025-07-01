92eee5534543843c0aadda5a6bc3a6c4b90041bc
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-01 20:24:55 +0000

    0.1.30

--- script/apt_upgrade.sh
+++ script/apt_upgrade.sh
@@ -2,6 +2,11 @@
 
 echo "[INFO] Upgrading apt packages"
 sudo apt-get update
+sudo apt-get install -y -qq \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 sudo apt-get upgrade -y
 
 echo "[INFO] apt packages upgraded!"
