0fdcc16e48ccf54a22ae4cf99e0e5151e1a1ef06
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-01 18:44:39 +0000

    0.1.27

--- script/qemu_agent.sh
+++ script/qemu_agent.sh
@@ -5,8 +5,4 @@ echo "[INFO] Installing qemu-guest-agent"
 sudo apt-get update
 sudo apt-get install -y qemu-guest-agent
 
-echo "[INFO] Enabling qemu-guest-agent service"
-sudo systemctl enable qemu-guest-agent
-sudo systemctl start qemu-guest-agent
-
 echo "[INFO] qemu-guest-agent installation complete!"
