68fa30eac0074c9287a3afea748ada21719cdf18
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-02 16:34:12 +0000

    0.1.35

--- script/apt_upgrade.sh
+++ script/apt_upgrade.sh
@@ -19,21 +19,21 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || echo "[ERROR] Failed to set up the Docker repository."
 
-echo "[INFO] Installing Docker CE and related components..."
-sudo apt-get update -qq || echo "[ERROR] Failed to update package lists after adding Docker repository."
-sudo apt-get install -y -qq \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-compose-plugin || echo "[ERROR] Failed to install Docker CE and related components."
+# echo "[INFO] Installing Docker CE and related components..."
+# sudo apt-get update -qq || echo "[ERROR] Failed to update package lists after adding Docker repository."
+# sudo apt-get install -y -qq \
+#     docker-ce \
+#     docker-ce-cli \
+#     containerd.io \
+#     docker-compose-plugin || echo "[ERROR] Failed to install Docker CE and related components."
 
-echo "[INFO] Verifying Docker installation..."
-if command -v docker &> /dev/null; then
-    docker --version
+# echo "[INFO] Verifying Docker installation..."
+# if command -v docker &> /dev/null; then
+#     docker --version
     
-    echo "[INFO] Docker installed successfully!"
-else
-    echo "[ERROR] Docker installation failed."
-fi
+#     echo "[INFO] Docker installed successfully!"
+# else
+#     echo "[ERROR] Docker installation failed."
+# fi
 
-echo "[INFO] Docker CE installation complete!"
+# echo "[INFO] Docker CE installation complete!"
