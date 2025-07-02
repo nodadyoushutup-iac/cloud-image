360085cbd791d7880cad5d84ae260a75b9142ff6
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-02 18:38:29 +0000

    0.1.43

--- script/apt_upgrade.sh
+++ script/apt_upgrade.sh
@@ -10,14 +10,14 @@ sudo apt-get install -y -qq \
     gnupg \
     lsb-release || echo "[ERROR] Failed to install prerequisites."
 
-# echo "[INFO] Adding Docker's GPG key..."
-# sudo mkdir -p /etc/apt/keyrings || echo "[ERROR] Failed to create /etc/apt/keyrings directory."
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || echo "[ERROR] Failed to add Docker's GPG key."
+echo "[INFO] Adding Docker's GPG key..."
+sudo mkdir -p /etc/apt/keyrings || echo "[ERROR] Failed to create /etc/apt/keyrings directory."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || echo "[ERROR] Failed to add Docker's GPG key."
 
-# echo "[INFO] Setting up the Docker repository..."
-# echo \
-#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || echo "[ERROR] Failed to set up the Docker repository."
+echo "[INFO] Setting up the Docker repository..."
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || echo "[ERROR] Failed to set up the Docker repository."
 
 # # echo "[INFO] Installing Docker CE and related components..."
 # sudo apt-get update -qq || echo "[ERROR] Failed to update package lists after adding Docker repository."
