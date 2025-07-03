b698bfd16dc9a24e3580033171d3a085e6a36820
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-03 22:51:50 +0000

    update apt list

--- script/apt.sh
+++ script/apt.sh
@@ -7,27 +7,41 @@ sudo apt-get update
 
 echo "[INFO] Installing apt packages..."
 sudo apt-get install -y \
-    age \
+    bat \
     bridge-utils \
+    btop \
     cpu-checker \
     curl \
     dnsutils \
+    duf \
+    ethtool \
+    fd-find \
     gh \
     git \
     htop \
     ifupdown \
+    iotop \
+    iperf3 \
     iptables \
     jq \
     libvirt-clients \
     libvirt-daemon-system \
+    lshw \
+    lsblk \
     lsof \
     make \
     mysql-client-core-8.0 \
     nano \
     net-tools \
+    netcat \
+    neovim \
     nfs-common \
     nmap \
+    ncat \
+    nping \
+    nvtop \
     open-iscsi \
+    parted \
     postgresql-client \
     python3 \
     python3-pip \
@@ -36,19 +50,25 @@ sudo apt-get install -y \
     qemu-kvm \
     qemu-system \
     qemu-system-x86 \
+    ripgrep \
+    rsync \
     screen \
+    smartmontools \
     strace \
     tcpdump \
     tmux \
     traceroute \
     tree \
+    ufw \
     unzip \
     vim \
     virtinst \
     wget \
     whois \
     xorriso \
-    zip
+    yq \
+    zip || echo "[ERROR] Failed to install one or more apt packages."
+
 
 echo "[INFO] Upgrading apt packages..."
 sudo apt-get upgrade -y
