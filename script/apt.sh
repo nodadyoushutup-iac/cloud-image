9b38b797f3e1c5aae4e11707d2dcc4acb4b78937
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-03 22:14:03 +0000

    apt

--- script/apt.sh
+++ script/apt.sh
@@ -5,19 +5,21 @@ echo "[INFO] Starting apt update, upgrade, and package installation..."
 echo "[INFO] Updating apt cache..."
 sudo apt-get update -qq || echo "[ERROR] Failed to update apt cache."
 
-echo "[INFO] Upgrading apt packages..."
-sudo apt-get upgrade -y -qq || echo "[ERROR] Failed to upgrade apt packages."
-
 echo "[INFO] Installing apt packages..."
 sudo apt-get install -y -qq \
     age \
+    bridge-utils \
+    cpu-checker \
     curl \
     dnsutils \
+    gh \
     git \
     htop \
     ifupdown \
     iptables \
     jq \
+    libvirt-clients \
+    libvirt-daemon-system \
     lsof \
     make \
     mysql-client-core-8.0 \
@@ -31,7 +33,9 @@ sudo apt-get install -y -qq \
     python3-pip \
     python3-venv \
     qemu-guest-agent \
+    qemu-kvm \
     qemu-system \
+    qemu-system-x86 \
     screen \
     strace \
     tcpdump \
@@ -40,9 +44,13 @@ sudo apt-get install -y -qq \
     tree \
     unzip \
     vim \
+    virtinst \
     wget \
     whois \
     xorriso \
     zip || echo "[ERROR] Failed to install one or more apt packages."
+
+echo "[INFO] Upgrading apt packages..."
+sudo apt-get upgrade -y -qq || echo "[ERROR] Failed to upgrade apt packages."
 
 echo "[INFO] All tasks completed successfully!"
