d9e606076c98a6e6ff66632b87cc2e23784320f1
nodadyoushutup <admin@nodadyoushutup.com>
2025-07-06 02:34:55 +0000

    script dir

--- script/script_dir.sh
+++ script/script_dir.sh
@@ -5,11 +5,9 @@ echo "[INFO] Starting script_dir..."
 echo "[INFO] Creating /script dir..."
 sudo mkdir -p /script
 
-echo "[INFO] Setting /script permissions..."
-sudo chmod -R 775 /script
-
 echo "[INFO] Moving scripts from /tmp..."
 sudo mv /tmp/register_github_public_key.sh /script/register_github_public_key.sh
 
 echo "[INFO] Setting /script permissions..."
 sudo chmod -R 775 /script
+sudo chown -R root:root /script
