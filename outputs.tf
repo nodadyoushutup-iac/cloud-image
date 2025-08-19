953ea456c9bce4e006ed69c30a64e9b99eae08f6
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2025-08-18 23:01:58 -0400

    Revert "Define structured variables for cloud-init and virtual machines"

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloud_init_yamldecoded" {
-  description = "Cloud-init variable after YAML roundtrip"
-  value       = yamldecode(yamlencode(var.cloud_init))
-}
-
-output "virtual_machine_yamldecoded" {
-  description = "Virtual machine variable after YAML roundtrip"
-  value       = yamldecode(yamlencode(var.virtual_machine))
-}
